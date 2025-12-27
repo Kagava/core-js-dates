1b417c126c5072f9ca57bd4b168284ff7fe2040f
Kagava <esspakov@mail.ru>
2025-12-27 14:06:18 +0700

    feat: complete getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,15 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const actualHours = hours > 9 ? `${hours}` : `0${hours}`;
+  const actualMinutes = minutes > 9 ? `${minutes}` : `0${minutes}`;
+  const actualSeconds = seconds > 9 ? `${seconds}` : `0${seconds}`;
+
+  return `${actualHours}:${actualMinutes}:${actualSeconds}`;
 }
 
 /**
